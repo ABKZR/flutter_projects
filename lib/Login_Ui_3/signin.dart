20a44ea7f8d26aaf11d0a2385e766429579c443b
Abdullah Bilal <33805512+ABKZR@users.noreply.github.com>
2021-06-08 00:07:23 +0500

    login

--- lib/Login_Ui_3/signin.dart
+++ lib/Login_Ui_3/signin.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class Signin extends StatefulWidget {
   const Signin({Key? key}) : super(key: key);
@@ -10,7 +11,42 @@ class _SigninState extends State<Signin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        leading: IconButton(onPressed: ()=>Navigator.pop(context), icon: Icon(Icons.arrow_back_ios,size: 20,color: Colors.black,)),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            alignment: AlignmentDirectional.bottomEnd,
+            image: AssetImage('assets/images/background-1.png'),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Text('Login',style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),),
+                SizedBox(height: 20,),
+                Text("Login Your Account", style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey.shade700,
+                ),)
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
