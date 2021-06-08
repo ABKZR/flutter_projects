9a5e610f1f9bfcc6280287549c70b1501b0027ac
Abdullah Bilal <33805512+ABKZR@users.noreply.github.com>
2021-06-08 15:08:29 +0500

    signup

--- lib/Login_Ui_3/signin.dart
+++ lib/Login_Ui_3/signin.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/Login_Ui_3/signup.dart';
 class Signin extends StatefulWidget {
   const Signin({Key? key}) : super(key: key);
 
@@ -22,31 +23,117 @@ class _SigninState extends State<Signin> {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            alignment: AlignmentDirectional.bottomEnd,
-            image: AssetImage('assets/images/background-1.png'),
-          ),
-        ),
+
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              children: [
-                Text('Login',style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),),
-                SizedBox(height: 20,),
-                Text("Login Your Account", style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey.shade700,
-                ),)
-              ],
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Text('Login',style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                      SizedBox(height: 20,),
+                      Text("Login Your Account", style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey.shade700,
+                      ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Column(
+                      children: [
+                        makeItem(title: 'Email'),
+                        makeItem(title: 'Password',obscureText: true),
+                      ],
+                    )),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Container(
+                    padding: EdgeInsets.only(top: 3,left: 3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border(
+                        left: BorderSide(color: Colors.black),
+                        right: BorderSide(color: Colors.black),
+                        top: BorderSide(color: Colors.black),
+                        bottom: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    child: MaterialButton(
+                      onPressed: (){},
+                      minWidth: double.infinity,
+                      height: 60,
+                      color: Colors.greenAccent,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Text('Login',style: TextStyle(fontSize: 18,fontWeight: FontWeight.w600),),
+                    ),
+                  ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have account?"),
+                      TextButton(child: Text("Sign up", style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      color: Colors.black87),
+                      ),
+                        onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>Signup()));
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height/3,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background-1.png'),
+                  fit: BoxFit.cover
+                ),
+              ),
             ),
           ],
         ),
       ),
+    );
+  }
+  Widget makeItem({title, obscureText=false}){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title,style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 15,
+          color: Colors.black87,
+        ),),
+        SizedBox(height: 5,),
+        TextField(
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 0),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey.shade400)
+            ),
+          ),
+        ),
+        SizedBox(height: 30,),
+      ],
     );
   }
 }
--- lib/Login_Ui_3/signup.dart
+++ lib/Login_Ui_3/signup.dart
@@ -9,6 +9,78 @@ class Signup extends StatefulWidget {
 class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        brightness: Brightness.light,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(onPressed: (){
+          Navigator.pop(context);
+        },
+          icon: Icon(Icons.arrow_back_ios,color: Colors.black,size: 20,),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(height: 30,),
+                Text("SignUp",style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+                ),),
+                SizedBox(height: 20,),
+                Text("Create an account, It's free",style: TextStyle(
+                  color: Colors.grey.shade700,
+                  fontSize: 18
+                ),)
+              ],
+            ),
+            Padding(padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                children: [
+                  makeItem(title: 'Email'),
+                  makeItem(title: 'Password',obscureText: true),
+                  makeItem(title: 'Conform Password',obscureText: true)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget makeItem({title, obscureText=false}){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title,style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 15,
+          color: Colors.black87,
+        ),),
+        SizedBox(height: 5,),
+        TextField(
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 0),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey.shade400)
+            ),
+          ),
+        ),
+        SizedBox(height: 30,),
+      ],
+    );
   }
 }
