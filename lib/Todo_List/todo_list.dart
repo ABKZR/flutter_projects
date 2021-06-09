89f296eaff2c8eb12bb801f26e884ed686bee73e
Abdullah Bilal <33805512+ABKZR@users.noreply.github.com>
2021-06-09 18:09:42 +0500

    debug

--- lib/Todo_List/todo_list.dart
+++ lib/Todo_List/todo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
 class TodoList extends StatelessWidget {
   const TodoList({Key? key}) : super(key: key);
@@ -27,46 +28,20 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List todos = [];
   String input = "";
-  @override
-  void initState() {
-    super.initState();
-    todos.add("Item1");
-    todos.add("Item2");
-    todos.add("Item3");
-    todos.add("Item4");
-  }
+  // createTodos(){
+  //   DocumentReference documentReference = FirebaseFirestore.instance.collection("Todo").doc(input);
+  //  Map<String, String> todos = {"todoTitle":input};
+  //  documentReference.set(todos).whenComplete(() => print("$input created"));
+  // }
+  // deleteTodos(){
+  //
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo List"),
-      ),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Dismissible(
-            key: Key(todos[index]),
-            child: Card(
-              elevation: 4,
-              margin: EdgeInsets.all(8),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)),
-              child: ListTile(
-                title: Text(todos[index]),
-                trailing: IconButton(
-                  icon: Icon(Icons.delete,color: Colors.red,),
-                onPressed: (){
-                    setState(() {
-                      todos.removeAt(index);
-                    });
-                },
-                ),
-              ),
-
-            ),
-          );
-        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(
@@ -99,6 +74,32 @@ class _HomeState extends State<Home> {
                   ],
                 );
               });
+        },
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Dismissible(
+            key: Key(todos[index]),
+            child: Card(
+              elevation: 4,
+              margin: EdgeInsets.all(8),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+              child: ListTile(
+                title: Text(todos[index]),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete,color: Colors.red,),
+                  onPressed: (){
+                    setState(() {
+                      todos.removeAt(index);
+                    });
+                  },
+                ),
+              ),
+
+            ),
+          );
         },
       ),
     );
