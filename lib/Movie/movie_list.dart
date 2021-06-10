8526a564cd9a32ebbc6d94038c48ff5110120a08
Abdullah Bilal <33805512+ABKZR@users.noreply.github.com>
2021-06-10 22:32:13 +0500

    update

--- lib/Movie/movie_list.dart
+++ lib/Movie/movie_list.dart
@@ -17,34 +17,74 @@ class MovieListView extends StatelessWidget {
       body: ListView.builder(
         itemCount: movieList.length,
         itemBuilder: (BuildContext context, int index){
-        return Card(
-          elevation: 4.5,
-          color: Colors.white,
-          child: ListTile(
-            title: Text(movieList[index].title),
-            subtitle: Text("${movieList[index].title}"),
-            leading: CircleAvatar(
-              backgroundColor: Colors.blue,
-              child: Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(movieList[index].images[0]),
-                    fit: BoxFit.cover
-                  ),
-                  borderRadius: BorderRadius.circular(13.9),
-                ),
-                child: null ,
-              ),
-            ),
-            trailing: Text("..."),
-            onTap: () {
-             Navigator.push(context, MaterialPageRoute(builder: (context) => MovieListViewDetails(movieName: movieList.elementAt(index).title,movie: movieList[index],)));
-            },
-          ),
-        );
+          return movieCard(movieList[index], context);
+        // return Card(
+        //   elevation: 4.5,
+        //   color: Colors.white,
+        //   child: ListTile(
+        //     title: Text(movieList[index].title),
+        //     subtitle: Text("${movieList[index].title}"),
+        //     leading: CircleAvatar(
+        //       backgroundColor: Colors.blue,
+        //       child: Container(
+        //         width: 200,
+        //         height: 200,
+        //         decoration: BoxDecoration(
+        //           image: DecorationImage(
+        //             image: NetworkImage(movieList[index].images[0]),
+        //             fit: BoxFit.cover
+        //           ),
+        //           borderRadius: BorderRadius.circular(13.9),
+        //         ),
+        //         child: null ,
+        //       ),
+        //     ),
+        //     trailing: Text("..."),
+        //     onTap: () {
+        //      Navigator.push(context, MaterialPageRoute(builder: (context) => MovieListViewDetails(movieName: movieList.elementAt(index).title,movie: movieList[index],)));
+        //     },
+        //   ),
+        // );
       },),
+    );
+  }
+  Widget movieCard(Movie movie, BuildContext context){
+    return InkWell(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 120,
+        child: Card(
+          color: Colors.black45,
+          child: Padding(
+            padding: EdgeInsets.only(top: 8, bottom: 8, left: 54),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(movie.title),
+                    Text("Rating: ${movie.imdbRating} /10")
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("Released: ${movie.released}"),
+                    Text(movie.runtime),
+                    Text(movie.rated)
+                  ],
+                )
+
+              ],
+            ),
+          ),
+        ),
+      ),
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => MovieListViewDetails(movieName: movie.title,movie: movie,)));
+      },
     );
   }
 }
@@ -58,7 +98,7 @@ final Movie movie;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueGrey.shade900,
-        title: Text("Movies ${this.movie.actors}"),
+        title: Text("Movies ${this.movie.title}"),
       ),
       body: Center(
         child: Container(
