3c24c2a0e6c30f733cd155be631f0a4b9475c2ba
Abdullah Bilal <33805512+ABKZR@users.noreply.github.com>
2021-06-24 17:24:17 +0500

    todoey

--- lib/Todoey/screens/tasks_screens.dart
+++ lib/Todoey/screens/tasks_screens.dart
@@ -1,25 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/Todoey/screens/add_tasks.dart';
+import 'package:flutter_project/Todoey/widgets/task_list.dart';
 
 class TaskScreen extends StatelessWidget {
-  const TaskScreen({ Key? key }) : super(key: key);
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(top: 60,left: 30,right: 30,bottom: 30),
-        child: Column(
-          children: [
-            CircleAvatar(
-              child: Icon(Icons.list,size: 30,color: Colors.lightBlueAccent,),
-              backgroundColor: Colors.white,
-              radius: 30,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(context: context, builder: (context) =>SingleChildScrollView(
+      child:Container(
+        padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: AddTaskScreen(),
+      )
+    ));
+        },
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ],
             ),
-            SizedBox(height: 10,),
-            Text('Todoey',style: TextStyle(color: Colors.white,fontSize: 50,fontWeight: FontWeight.w700),)
-          ],
-        ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20))),
+              child: TasksList(),
+            ),
+          )
+        ],
       ),
     );
   }
