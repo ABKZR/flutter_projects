a9795a8748957e8086835082b0eb0e91e7171548
Abdullah Bilal <33805512+ABKZR@users.noreply.github.com>
2021-06-30 19:26:18 +0500

    RandomWords

--- lib/word/word_generator.dart
+++ lib/word/word_generator.dart
@@ -1,7 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter_project/word/random_words.dart';
 
 class WordGenerator extends StatefulWidget {
   @override
@@ -13,49 +11,11 @@ class _WordGeneratorState extends State<WordGenerator> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Startup Name Generator',
+      theme: ThemeData( 
+        primaryColor: Colors.white,
+      ), 
       home: RandomWords(),
     );
   }
 }
 
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  Widget _buildSuggestions() {
-  return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return const Divider(); /*2*/
-
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return _buildRow(_suggestions[index]);
-      });
-}
-Widget _buildRow(WordPair pair) {
-  return ListTile(
-    title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ),
-  );
-}
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-    appBar: AppBar(
-      title: const Text('Startup Name Generator'),
-    ),
-    body: _buildSuggestions(),
-  );
-    }
-}
